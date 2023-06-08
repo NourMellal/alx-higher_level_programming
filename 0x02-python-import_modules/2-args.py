3265f5e218e0742f8e568db52a42cbdc8872ff93
NourMellal <mellal.nour9@gmail.com>
2023-06-08 16:41:49 +0100

    task2 inchallah

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    len_argv = len(argv) - 1
+
+    if len_argv == 0:
+        print("0 arguments.")
+    elif len_argv == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len_argv))
+    for i in range(len_argv):
+        print("{}: {}".format(i + 1, argv[i + 1]))
