6740ba8d0b2f0893b7e8c367a50b794c7fd46305
NourMellal <mellal.nour9@gmail.com>
2023-06-08 16:23:41 +0100

    /

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
 
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} \ {} = {}".format(a, b, div(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
     print("{} + {} = {}".format(a, b, add(a, b)))
