97f5c4887827090adc917cee06bbd257507ae4f7
NourMellal <mellal.nour9@gmail.com>
2023-06-08 19:39:58 +0100

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    ops = {"+": add, "-": sub, '*': mul, "/": div}
+
+    if argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))
