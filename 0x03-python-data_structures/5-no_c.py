c41ae21bfdcb9115a4ba5b419e3c069103b85f77
NourMellal <mellal.nour9@gmail.com>
2023-06-09 17:01:28 +0100

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    new_str = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            print(i)
+            new_str += i
+    return new_str
+
