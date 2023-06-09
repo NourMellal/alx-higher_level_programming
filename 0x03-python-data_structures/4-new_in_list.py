82bb4c30ded78624f4afffdb867df60bb9041313
NourMellal <mellal.nour9@gmail.com>
2023-06-09 16:35:59 +0100

    4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return element
+    tmp_list = my_list[:]
+    tmp_list[idx] = element
+    return tmp_list
