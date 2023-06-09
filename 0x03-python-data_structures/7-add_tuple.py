1dc190709bb9319dbf9dec3ca98f1a070896a047
NourMellal <mellal.nour9@gmail.com>
2023-06-09 21:29:32 +0100

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    return (tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1])
+
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
