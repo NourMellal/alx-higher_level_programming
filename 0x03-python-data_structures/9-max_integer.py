2589b181e0658d125eecbba6bc4e0b1daa8648c1
NourMellal <mellal.nour9@gmail.com>
2023-06-11 13:22:20 +0100

    get max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    comp = my_list[0]
+    for i in range(len(my_list)):
+        if comp < my_list[i]:
+            comp = my_list[i]
+    return comp
