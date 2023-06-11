c9b4fc89e4cff9234bbd98be0093cd2aae4920aa
NourMellal <mellal.nour9@gmail.com>
2023-06-11 13:45:51 +0100

    by two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        elif my_list[i] % 2 == 1:
+            new_list.append(False)
+    return new_list
