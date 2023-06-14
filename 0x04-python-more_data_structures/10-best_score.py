c0e1c76ed0bf488af2864535eba539a20203c6da
NourMellal <mellal.nour9@gmail.com>
2023-06-14 23:48:20 +0100

    10- high score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,11 +8,3 @@ def best_score(a_dictionary):
         if highest_score < a_dictionary[i]:
             highest_score = a_dictionary[i]
     return highest_score
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
