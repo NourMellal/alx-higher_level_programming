7c82b32260928e1f7322a2c54b4009c8068c19d0
NourMellal <mellal.nour9@gmail.com>
2023-06-15 01:14:23 +0100

    12-roman_to

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (not isinstance(roman_string, str) or roman_string == ""):
+        return 0
+
+    dic_roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    num = 0
+
+    for i in range(len(roman_string)):
+        if dic_roman.get(roman_string[i], 0) == 0:
+            return 0
+        if (i != (len(roman_string) - 1)) and\
+            dic_roman[roman_string[i]] < dic_roman[roman_string[i + 1]]:
+            num += dic_roman[roman_string[i]] * -1
+        else:
+            num += dic_roman[roman_string[i]]
+    return num
