e44dc7c8c22299244c206d1efb6dbd2cfc3bc675
NourMellal <mellal.nour9@gmail.com>
2023-06-28 02:52:13 +0100

    square size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define class square"""
+
+
+class Square:
+    """Define constructor size of a square"""
+    def __init__(self, size=0):
+        """initialization of a new square
+
+        size is the size of new aquare
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
