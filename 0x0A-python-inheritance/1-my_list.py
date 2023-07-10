8ff2fa77cb6c6284ddd03d1c941a4e6ff0652bd2
NourMellal <mellal.nour9@gmail.com>
2023-07-10 18:44:27 +0100

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class MyList(list):
+    """Subclass of list with an additional method to print
+    the list in sorted order."""
+
+    def print_sorted(self):
+        """
+        Prints the elements of the list in sorted (ascending) order.
+
+        Note that this does not modify the original list; it only prints
+        a sorted version.
+        """
+        print(sorted(self))
