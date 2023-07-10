fc9d1dffb53cc2c0706260351f8a39e0b5030210
NourMellal <mellal.nour9@gmail.com>
2023-07-10 20:33:01 +0100

    instance method that validate the value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class BaseGeometry:
+    """Represent base Geometry"""
+
+    def area(self):
+        """Not emplement"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ instance method that validate the value """
+        if not isinstance(int, value):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
