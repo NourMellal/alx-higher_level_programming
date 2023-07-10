3d7f11cf1af95af27a893deae477cd483efb125b
NourMellal <mellal.nour9@gmail.com>
2023-07-10 23:47:53 +0100

    rectangle description:

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    implement square class
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+"""
+    super class
+"""
+
+
+class Square(Rectangle):
+    """
+    implement square class
+    """
+
+    def __init__(self, size):
+        """Concontructor and width, height"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        strn = "[" + str(self.__class__.__name__) + "] "
+        strn += str(self.__size) + "/" + str(self.__size)
+        return strn
