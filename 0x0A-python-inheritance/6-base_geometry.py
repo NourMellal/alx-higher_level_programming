3a61dee031306bcc30017a6b4554c7f7258f1d2c
NourMellal <mellal.nour9@gmail.com>
2023-07-10 20:13:42 +0100

    Represent base Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class BaseGeometry:
+    """ Represent base Geometry """
+    def area(self):
+        """Not emplement """
+        raise Exception("area() is not implemented")
