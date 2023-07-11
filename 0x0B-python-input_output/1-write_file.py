a42a171cd2a3dd2081418cfd0f522f051ea8ff3b
NourMellal <mellal.nour9@gmail.com>
2023-07-11 21:25:15 +0100

    write a text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """ write a string in a file """
     with open(filename, "w", encoding='utf-8') as file:
-        print(file.write(text), end="")
+        print(file.write(text))
