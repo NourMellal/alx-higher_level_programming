5b4daa56623b718218494eee2a80342a659ab414
NourMellal <mellal.nour9@gmail.com>
2023-07-11 23:52:29 +0100

    append in the end

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" implement append_write function"""
+
+
+def append_write(filename="", text=""):
+    """append in the end of a text file """
+    with open(filename, encoding="utf8") as f:
+        return f.write(text)
