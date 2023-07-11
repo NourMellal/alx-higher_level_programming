d65b80be54c4a53d19e0e0399234a0fee5a5a500
NourMellal <mellal.nour9@gmail.com>
2023-07-11 21:20:14 +0100

    add docummentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""function that read from a txt file"""
+
+
 def read_file(filename=""):
     """ read file and print its content"""
     with open(filename, "r", encoding='utf-8') as file:
