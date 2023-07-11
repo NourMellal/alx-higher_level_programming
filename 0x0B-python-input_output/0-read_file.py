1a682bed04298bdd68f327abff01fcde6964d690
NourMellal <mellal.nour9@gmail.com>
2023-07-11 21:19:13 +0100

    file read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 def read_file(filename=""):
     """ read file and print its content"""
     with open(filename, "r", encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
