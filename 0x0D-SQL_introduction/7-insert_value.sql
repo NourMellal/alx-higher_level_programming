2ca68fad3b1d280a96e23d7e9ecae24c9999cd7f
NourMellal <mellal.nour9@gmail.com>
2023-08-16 02:30:16 +0100

    inserts a new row in the table first_table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- that inserts a new row in the table first_table (database hbtn_0c_0) in My MySQL server.
+-- inserts a new row in the table first_table (database hbtn_0c_0) in My MySQL server.
 INSERT INTO first_table(id, name)
 VALUES(89, "Best School")
