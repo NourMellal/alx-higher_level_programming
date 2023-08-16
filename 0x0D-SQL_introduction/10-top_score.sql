3a0f5160591db0bae11f51c4640ca4d713b45c19
NourMellal <mellal.nour9@gmail.com>
2023-08-16 14:44:05 +0100

    MAX

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- t lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT * FROM second_table
+SELECT score, name FROM second_table
 ORDER BY score DESC
