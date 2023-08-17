6e33c5a658999f8dfdd82e3ce6327b07a7efe5fb
NourMellal <mellal.nour9@gmail.com>
2023-08-17 03:41:32 +0100

    creates the table id_not_null on your MySQL server.

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
---creates the table unique_id on your MySQL server.
+-- creates the table unique_id on your MySQL server.
 CREATE TABLE IF NOT EXISTS unique_id(
     id INT DEFAULT 1,
-    name VARCHAR(256),
+    name VARCHAR(256) NOT NULL,
     UNIQUE(id)
 );
