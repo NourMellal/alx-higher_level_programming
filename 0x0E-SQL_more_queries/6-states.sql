e09e7cf1774f0ec2ea1dfaee4c5932e8ed8d209a
NourMellal <mellal.nour9@gmail.com>
2023-08-17 04:06:14 +0100

    creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on My MySQL server.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on My MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
+USE hbtn_0d_usa;
 CREATE TABLE
     IF NOT EXISTS states (
-        id INT AUTO_INCREMENT NOT NULL PRIMERY KEY UNIQUE,
-        name VARCHAR(256))
+        id INT AUTO_INCREMENT NOT NULL UNIQUE,
+        name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+
